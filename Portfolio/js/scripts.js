b9154092c369bd6d67054f39108a87ee564c5da6
Jonathan Nguyen <Jonathan.nguyen2021@gmail.com>
2021-08-12 13:17:20 -0700

    8/12/2021 1:17p,

--- Portfolio/js/scripts.js
+++ Portfolio/js/scripts.js
@@ -1,3 +1,5 @@
+
+//Navbar Visibility
 window.onscroll = function() {scroll()};
 
 var landing = document.getElementById("landingPage");
@@ -10,4 +12,66 @@ function scroll(){
     else{
         document.getElementById("navbar").style.top = "-100px";
     }
+}
+
+//Switch Skills Active Buttons
+var btnContainer = document.getElementById("buttonsContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    if(this.className != "btn color-1 active"){
+        for(var j=0; j<btns.length;j++){
+            if(btns[j].className == "btn color-1 active"){
+                btns[j].className = "btn color-1"
+            }
+        }
+        this.className +=  " active";
+    }
+    var languages = document.getElementsByClassName("language");
+    var software = document.getElementsByClassName("software");
+    var other = document.getElementsByClassName("other");
+
+    if(document.getElementById("language").className == "btn color-1 active"){
+        for(var x=0;x<languages.length;x++){
+            languages[x].style.display = "flex";
+        }
+        for(var x=0;x<software.length;x++){
+            software[x].style.display = "none";
+        }
+        for(var x=0;x<other.length;x++){
+            other[x].style.display = "none";
+        }
+    }
+    else if(document.getElementById("software").className == "btn color-1 active"){
+       for(var x=0;x<languages.length;x++){
+            languages[x].style.display = "none";
+        }
+        for(var x=0;x<software.length;x++){
+            software[x].style.display = "flex";
+        }
+        for(var x=0;x<other.length;x++){
+            other[x].style.display = "none";
+        }
+    }
+    else{
+        for(var x=0;x<languages.length;x++){
+            languages[x].style.display = "none";
+        }
+        for(var x=0;x<software.length;x++){
+            software[x].style.display = "none";
+        }
+        for(var x=0;x<other.length;x++){
+            other[x].style.display = "flex";
+        }
+    }
+  });
+}
+
+//Changes Skill section layout
+var width = window.innerHeight;
+if(width <= 950){
+    document.getElementById("buttonsContainer").className = "btn-group btn-group-md"
+}
+else{
+    document.getElementById("buttonsContainer").className = "btn-group-vertical btn-group-lg"
 }
